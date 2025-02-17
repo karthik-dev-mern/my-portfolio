065297952b990a2017b684205a99e3f1fd0d29dc
karthik <prokarthik1449@gmail.com>
2025-02-18 03:10:58 +0530

    Initial commit

---
+++ src/components/Skillset/Techstack.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiJavascript1,
+  DiReact,
+  DiFirebase,
+  DiBootstrap,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiMaterialui,
+  SiRedux,
+  SiHtml5
+} from "react-icons/si";
+
+const Techstack = () => {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMaterialui />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
